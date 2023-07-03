5e0e37c5e4886d3375e08b012fc6def866e32212
SenanTech <noelieadjile@gmail.com>
2023-07-03 18:09:56 +0100

    integration de template

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('template');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/first', function () {
+    return view('fichier');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
+
